6bab0307957b9940939be1ed66a128876482e68c
CodingMauri <106369465+CodingMauri@users.noreply.github.com>
2022-09-03 13:11:43 -0700

    Initial Commit

---
+++ src/Components/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Hero from "./Hero";
+import Nav from "./Nav";
+
+export default function Home() {
+  return (
+    <div>
+      <Nav />
+      <Hero />
+     
+    </div>
+  );
+}
