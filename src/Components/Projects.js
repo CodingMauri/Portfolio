ee00eabb904a683a533297fed50659ae68e4e509
CodingMauri <106369465+CodingMauri@users.noreply.github.com>
2022-09-08 10:37:00 -0700

    deployment

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Card, Row } from "react-bootstrap";
+import { Container, } from "react-bootstrap";
 import ProjectCard from "./ProjectCard";
 const Projects = () => {
   const projectData = [
